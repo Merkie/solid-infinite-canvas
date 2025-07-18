3466b2bdd3c2eaaa9515ede1c200be0adcb482bc
merkie <merkie@post.com>
2025-07-18 14:21:44 -0500

    Refactor connection management: consolidate connection logic into ConnectionsPlugin, add ElementConnectionPoint component for rendering connection points.

--- src/index.tsx
+++ src/index.tsx
@@ -658,38 +658,6 @@ const ElementRenderer: ElementRendererComponent = props => {
   )
 }
 
-export const ElementConnectionPoint: Component<{
-  elementId: string
-  type: 'input' | 'output'
-}> = props => {
-  const { state } = useStage()
-  const element = state.elements[props.elementId]
-
-  return (
-    <Show when={element}>
-      <div
-        data-sic-type="connection-point"
-        data-element-id={props.elementId}
-        data-connection-point={props.type}
-        style={{
-          position: 'absolute',
-          width: '13px',
-          height: '13px',
-          'background-color': props.type === 'input' ? 'orange' : 'blue',
-          'border-radius': '50%',
-          left: props.type === 'input' ? '0px' : '100%',
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
-          border: '2px solid black',
-          'box-sizing': 'border-box',
-          'pointer-events': 'all',
-          cursor: 'pointer',
-        }}
-      />
-    </Show>
-  )
-}
-
 export function createInitialState(elements: UncreatedElementState[]) {
   const initialState = elements.reduce(
     (acc, el) => {
